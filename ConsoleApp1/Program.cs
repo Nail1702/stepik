2e9275352aec877fab10080216c8fc9e4890baf2
Nail1702 <ahunovnail80@gmail.com>
2026-04-25 21:24:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using ConsoleApp1.Models;
+using ConsoleApp1.Services;
+using MySql.Data.MySqlClient;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Database=stepik;Uid=root;Pwd=260808AnAnAn;";
+
+        // Использование блока using для автоматического закрытия соединения
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            // Открытие соединения
+            connection.Open();
+            Console.WriteLine("Подключение открыто");
+
+            // Вывод информации о подключении
+            PrintProperties(connection);
+        }
+        Console.WriteLine("Подключение закрыто");
+    }
+
+    // Метод для вывода всех свойств объекта
+    private static void PrintProperties(object obj)
+    {
+        // Получение типа объекта
+        Type type = obj.GetType();
+        // Получение всех свойств объекта
+        PropertyInfo[] properties = type.GetProperties();
+
+        // Перебор всех свойств
+        foreach (var property in properties)
+        {
+            // Получение значения свойства
+            object value = property.GetValue(obj, null);
+            // Вывод имени и значения свойства
+            Console.WriteLine($"{property.Name}: {value}");
+
+        }
+    }
+
+    public static void RegisterUser()
+    {
+        Console.WriteLine("Введите имя и фамилию через пробел и нажмите Enter:");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.Write("Произошла ошибка, произведен выход на главную страницу\n");
+            return;
+        }
+
+        string[] parts = input.Split(new[] { ' ' }, 2);
+        if (parts.Length < 2)
+        {
+            Console.Write("Произошла ошибка, произведен выход на главную страницу\n");
+            return;
+        }
+
+        string firstName = parts[0];
+        string lastName = parts[1];
+
+        // Создание объекта User и заполнение обязательного поля FullName
+        User user = new User();
+        user.FullName = $"{firstName} {lastName}";
+
+        bool result = UsersService.Add(user);
+
+        if (result)
+        {
+            Console.Write($"Пользователь '{firstName} {lastName}' успешно добавлен.\n");
+        }
+        else
+        {
+            Console.Write("Произошла ошибка, произведен выход на главную страницу\n");
+        }
+    }
+}
---
+++ ConsoleApp1/Services/UsersService.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Services
+{
+    public class UsersService
+    {
+        public static bool Add(User user)
+        {
+            try
+            {
+                string connectionString = Constant.ConnectionString;
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sqlQuery = "INSERT INTO users (FullName, Details, JoinDate, Avatar, IsActive) VALUES (@FullName, @Details, @JoinDate, @Avatar, @IsActive)";
+                    using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@FullName", user.FullName);
+                        command.Parameters.AddWithValue("@Details", user.Details);
+                        command.Parameters.AddWithValue("@JoinDate", user.JoinDate);
+                        command.Parameters.AddWithValue("@Avatar", user.Avatar);
+                        command.Parameters.AddWithValue("@IsActive", user.IsActive);
+                        int rowsAffected = command.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
